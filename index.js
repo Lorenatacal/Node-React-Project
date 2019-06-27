7c84ccd013b88dee455aab1eb47133d997df41e6
Lorenatacal <Lorenatacal@gmail.com>
2019-06-27 22:40:29 +0100

    Change greeting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require ('express');
 
-const app = express();
+const app = express ();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+app.get ('/', (req, res) => {
+  res.send ({message: 'Hello everyone'});
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen (PORT);
