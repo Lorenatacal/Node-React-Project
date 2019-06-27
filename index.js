98a52a937d57862adeb0126dbaee83698899af12
Lorenatacal <Lorenatacal@gmail.com>
2019-06-27 23:16:29 +0100

    Install passport and passport-google-oauth20

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require ('express');
+const passport = require ('passport');
+const GoogleStrategy = require ('passport-google-oauth20').Strategy;
 
 const app = express ();
-
-app.get ('/', (req, res) => {
-  res.send ({message: 'Hello everyone'});
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen (PORT);
